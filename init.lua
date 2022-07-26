2676e74a335c6685680ad8b4a066a59e330ef35e
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-07-26 23:39:18 +0200

    Initial Commit
    It can do stuff... sorta

---
+++ init.lua
@@ -0,0 +1,69 @@
+
+permatime = {
+	override = nil
+}
+
+local storage = minetest.get_mod_storage()
+
+local function time_to_override(time)
+	--return time+0.5
+	if time + 0.5 > 1 then
+		return time
+	else
+		return time + 0.5
+	end
+end
+
+local function set_override(timeofday)
+	if timeofday < 0 or timeofday > 1 then return end
+
+	permatime.override = timeofday
+	storage:set_float('permatime', timeofday)
+	print(timeofday)
+	print(time_to_override(timeofday))
+
+	for _, player in pairs(minetest.get_connected_players()) do
+		player:override_day_night_ratio(time_to_override(timeofday))
+	end
+end
+
+function update_time()
+	if permatime.override then
+		minetest.set_timeofday(permatime.override)
+	end
+
+	minetest.after(1, update_time)
+end
+
+minetest.register_on_mods_loaded(function()
+	permatime.override = storage:get('permatime')
+
+	minetest.after(1, update_time)
+end)
+
+minetest.register_chatcommand("permatime", {
+	params = "<0..23>:<0..59>",
+	description = "Set the permanent time",
+	privs = {},
+	func = function(name, param)
+		if not core.get_player_privs(name).settime then
+			return false, "You don't have permission to run this command (missing privilege: settime)."
+		end
+
+		local hour, minute = param:match("^(%d+):(%d+)$")
+		hour = tonumber(hour); minute = tonumber(minute)
+
+		if hour < 0 or hour > 23 then
+			return false, "Invalid hour (must be between 0 and 23 inclusive)."
+		elseif minute < 0 or minute > 59 then
+			return false, "Invalid minute (must be between 0 and 59 inclusive)."
+		end
+
+		local timeofday = (hour * 60 + minute) / 1440
+		local player = minetest.get_player_by_name(name)
+
+		set_override(timeofday)
+
+		return true, "Time of day changed."
+	end,
+})
